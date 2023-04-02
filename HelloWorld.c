adc30d4fb2316955f28a423b138398dc2344abd5
Esan Millet <esanmillet101@gmail.com>
2023-04-02 17:42:57 -0400

    Hello Crazy World!

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,11 +3,7 @@
 //-----------------------------------------------------------------------------
 int main(void) {
 
-<<<<<<< HEAD
-    printf("Hello Very Crool World!\n");
-=======
-    printf("Hello Very Happy World!\n");
->>>>>>> happy
+	printf("Hello Crazy World!");
 
     return(EXIT_SUCCESS);
 }
